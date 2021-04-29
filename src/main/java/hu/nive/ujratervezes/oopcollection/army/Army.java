205ec07cc14f48e45ae0cd6f1b8c04eac3977694
Adam Hamar <adam.hamar@gmail.com>
2021-04-29 11:27:03 +0200

    Implement the Army class

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/Army.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/Army.java
@@ -1,3 +1,26 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public class Army {}
+import java.util.ArrayList;
+import java.util.List;
+
+public class Army {
+    private final List<MilitaryUnit> militaryUnits = new ArrayList<>();
+    
+    public void addUnit(MilitaryUnit militaryUnit) {
+        militaryUnits.add(militaryUnit);
+    }
+    
+    public void damageAll(int damage) {
+        militaryUnits.forEach(militaryUnit -> militaryUnit.sufferDamage(damage));
+        militaryUnits.removeIf(militaryUnit -> militaryUnit.getHitPoints() < 25);
+    }
+    
+    public int getArmyDamage() {
+        return
+            militaryUnits.stream().mapToInt(MilitaryUnit::doDamage).sum();
+    }
+    
+    public int getArmySize() {
+        return militaryUnits.size();
+    }
+}
