97c3d6bda9b3416f6dbf3b03234c58fdefeba059
Adam Hamar <adam.hamar@gmail.com>
2021-04-29 11:24:40 +0200

    Implement the Archer class

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/Archer.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/Archer.java
@@ -1,3 +1,17 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public class Archer {}
+public class Archer extends MilitaryUnit {
+    public Archer() {
+        super(false, 50, 20);
+    }
+    
+    @Override
+    public int doDamage() {
+        return getDamage();
+    }
+    
+    @Override
+    public void sufferDamage(int damage) {
+        setDamage(damage);
+    }
+}
