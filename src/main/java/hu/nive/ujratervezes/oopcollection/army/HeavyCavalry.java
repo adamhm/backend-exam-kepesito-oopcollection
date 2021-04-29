240d8cda941ee58876253ae629d367e415b58dd4
Adam Hamar <adam.hamar@gmail.com>
2021-04-29 11:25:47 +0200

    Implement the HeavyCavalry class

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/HeavyCavalry.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/HeavyCavalry.java
@@ -1,3 +1,25 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public class HeavyCavalry {}
+public class HeavyCavalry extends MilitaryUnit {
+    private boolean firstAttack = true;
+    
+    public HeavyCavalry() {
+        super(true, 150, 20);
+    }
+    
+    @Override
+    public int doDamage() {
+        if (firstAttack) {
+            firstAttack = false;
+            
+            return getDamage() * 3;
+        }
+        
+        return getDamage();
+    }
+    
+    @Override
+    public void sufferDamage(int damage) {
+        setDamage(damage);
+    }
+}
