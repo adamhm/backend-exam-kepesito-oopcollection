36b33e929c85ec80824199cb1c9f30caefdc026e
Adam Hamar <adam.hamar@gmail.com>
2021-04-29 11:26:33 +0200

    Implement the Swordsman class

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/Swordsman.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/Swordsman.java
@@ -1,3 +1,23 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public class Swordsman {}
+public class Swordsman extends MilitaryUnit {
+    private boolean firstAttack = true;
+    
+    public Swordsman(boolean armored) {
+        super(armored, 100, 10);
+    }
+    
+    @Override
+    public int doDamage() {
+        return getDamage();
+    }
+    
+    @Override
+    public void sufferDamage(int damage) {
+        if (!firstAttack) {
+            setDamage(damage);
+        }
+        
+        firstAttack = false;
+    }
+}
