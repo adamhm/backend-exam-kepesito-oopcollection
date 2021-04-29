e872ea5c648f2830e4049f038b14b27ce364cabf
Adam Hamar <adam.hamar@gmail.com>
2021-04-29 11:23:34 +0200

    Implement the MilitaryUnit class

--- src/main/java/hu/nive/ujratervezes/oopcollection/army/MilitaryUnit.java
+++ src/main/java/hu/nive/ujratervezes/oopcollection/army/MilitaryUnit.java
@@ -1,3 +1,33 @@
 package hu.nive.ujratervezes.oopcollection.army;
 
-public abstract class MilitaryUnit {}
+public abstract class MilitaryUnit {
+    private final boolean armored;
+    private final int damage;
+    private int hitPoints;
+    
+    public MilitaryUnit(boolean armored, int hitPoints, int damage) {
+        this.armored = armored;
+        this.hitPoints = hitPoints;
+        this.damage = damage;
+    }
+    
+    public boolean isArmored() {
+        return armored;
+    }
+    
+    public int getHitPoints() {
+        return hitPoints;
+    }
+    
+    public int getDamage() {
+        return damage;
+    }
+    
+    public void setDamage(int damage) {
+        hitPoints -= isArmored() ? damage / 2 : damage;
+    }
+    
+    public abstract int doDamage();
+    
+    public abstract void sufferDamage(int damage);
+}
